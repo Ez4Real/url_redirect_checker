ec25558503e19ddff662b63b7e60e6f0b61db807
ez4real <butilka05roma@gmail.com>
2024-04-04 18:32:49 -0400

    add multithreading

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import re
 import csv
 import requests
 from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor
 
 def check_redirect(domain):
     url = 'http://' + domain
@@ -11,10 +12,8 @@ def check_redirect(domain):
         final_url = urlparse(response.url).netloc
         print("Current URL: ", final_url)
         if domain != final_url:
-            print('Redirect from ', domain, ' to ', final_url)
-            return final_url, "Redirected"
+            return final_url, "Redirected successfully"
         else:
-            print('No redirect from ', domain)
             return None, "No Redirection"
     except requests.exceptions.RequestException as e:
         host_match = re.search(r"host='(.*?)'", str(e))
@@ -26,15 +25,17 @@ def main():
     with open('domains.txt', 'r') as file:
         domains = file.read().splitlines()
 
-    with open('output.csv', 'w', newline='') as csvfile:
+    with open('input.csv', 'w', newline='') as csvfile:
         fieldnames = ['Domain', 'Redirect URL', 'Status']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for index, domain in enumerate(domains):
-            print(index+1, ' - ', domain)
-            redirect_url, status = check_redirect(domain)
-            writer.writerow({'Domain': domain, 'Redirect URL': redirect_url, 'Status': status})
+        with ThreadPoolExecutor(max_workers=50) as executor:
+            futures = [executor.submit(check_redirect, domain) for domain in domains]
+            for index, future in enumerate(futures):
+                domain = domains[index]
+                redirect_url, status = future.result()
+                writer.writerow({'Domain': domain, 'Redirect URL': redirect_url, 'Status': status})
 
 if __name__ == "__main__":
     main()
